157097d6050100d9514446d41940954288fa248e
AT <libra.giocondo@gmail.com>
2021-05-22 13:05:44 +0300

    feat: execute exercise 1

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,48 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    if (monthNumber >= 1 && monthNumber <=12) {
+        NSDateFormatter *df = [[NSDateFormatter alloc] init];
+        NSString *monthName = [[df monthSymbols] objectAtIndex:(monthNumber-1)];
+        return monthName;
+    } else {
+        return  nil;
+    }
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z"];
+    NSDate *dateDay = [dateFormatter dateFromString:date];
+
+    NSCalendar * calendar = [NSCalendar currentCalendar];
+    NSDateComponents * dateComponents = [calendar components: NSCalendarUnitDay | NSCalendarUnitWeekday fromDate: dateDay];
+
+    return (long)dateComponents.day;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSLocale *deLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    NSDateFormatter *weekday = [[NSDateFormatter alloc] init];
+    [weekday setLocale:deLocale];
+    [weekday setDateFormat: @"EE"];
+    return [weekday stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSDate *startDate = nil;
+    NSTimeInterval interval = 0.0;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    [calendar rangeOfUnit:NSCalendarUnitWeekOfYear startDate: &startDate interval: &interval forDate: [NSDate date]];
+    NSDate *endDate = [calendar dateByAddingUnit:NSCalendarUnitSecond value: (NSInteger)interval toDate: startDate options:NSCalendarMatchNextTime];
+
+    return [date compare:startDate] == NSOrderedDescending && [date compare:endDate] == NSOrderedAscending;
 }
 
 @end
