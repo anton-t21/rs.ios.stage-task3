453ea82a210ed15071d72f541d44665544590fa0
AT <libra.giocondo@gmail.com>
2021-05-22 15:36:03 +0300

    feat: execute exercise 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,23 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int n = 0;
+    int arrayCount = (int)ratingArray.count;
+
+    for (int i = 0; i < arrayCount - 2; i++){
+        for (int j = i + 1; j < arrayCount - 1; j++){
+            for (int k = j + 1; k <= arrayCount -1; k++) {
+                int a = ratingArray[i].intValue;
+                int b = ratingArray[j].intValue;
+                int c = ratingArray[k].intValue;
+
+                if ((a < b && b < c) || (a > b && b > c)){
+                    n++;
+                }
+            }
+        }
+    }
+    return n;
 }
 
 @end
