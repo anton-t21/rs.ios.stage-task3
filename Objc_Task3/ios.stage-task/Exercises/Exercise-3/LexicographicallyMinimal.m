101cee3f0755b31201582c04aba7875d036ef85f
AT <libra.giocondo@gmail.com>
2021-05-22 23:17:16 +0300

    feat: execute exercise 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,43 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSString *result = @"";
+    NSString *firstStringtLetter = @"";
+    NSString *secondStringtLetter = @"";
+
+    NSString *stringOne = string1;
+    NSString *stringTwo = string2;
+
+
+    int firstStringLength = (int)[stringOne length];
+    int secondStringLength = (int)[stringTwo length];
+
+    while (firstStringLength != 0 && secondStringLength != 0) {
+        firstStringLength = (int)[stringOne length];
+        secondStringLength = (int)[stringTwo length];
+        if (firstStringLength != 0){
+            firstStringtLetter = [stringOne substringToIndex:1];
+        }
+        if (secondStringLength != 0) {
+            secondStringtLetter = [stringTwo substringToIndex:1];
+        }
+        if (firstStringLength != 0 && secondStringLength != 0) {
+            if ([stringTwo characterAtIndex:0] >= [stringOne characterAtIndex:0]) {
+                result = [result stringByAppendingString:firstStringtLetter];
+                stringOne = [stringOne substringFromIndex:1];
+            } else {
+                result = [result stringByAppendingString:secondStringtLetter];
+                stringTwo = [stringTwo substringFromIndex:1];
+            }
+        } else if (firstStringLength != 0) {
+            result = [result stringByAppendingString:stringOne];
+            break;
+        } else {
+            result = [result stringByAppendingString:stringTwo];
+            break;
+        }
+    }
+    return result;
 }
 
 @end
